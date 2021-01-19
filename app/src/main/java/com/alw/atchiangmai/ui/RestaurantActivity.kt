bcbd18295083b31a68281d0b6c45ae78c616641c
TeemMarutthep <teemMarutthep@gmail.com>
2021-01-19 17:30:16 +0700

    Search in food

--- app/src/main/java/com/alw/atchiangmai/ui/RestaurantActivity.kt
+++ app/src/main/java/com/alw/atchiangmai/ui/RestaurantActivity.kt
@@ -2,26 +2,69 @@ package com.alw.atchiangmai.ui
 
 import android.app.ProgressDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.alw.atchiangmai.Adapter.RestaurantAdapter
 import com.alw.atchiangmai.Adapter.onCLickAdapterListener
-import com.alw.atchiangmai.FirebaseController
 import com.alw.atchiangmai.FirebaseController.Firebase.db
-import com.alw.atchiangmai.Model.ItemDataFeed
 import com.alw.atchiangmai.Model.RestaurantData
 import com.alw.atchiangmai.R
 import kotlinx.android.synthetic.main.activity_restaurant.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class RestaurantActivity : AppCompatActivity(), onCLickAdapterListener {
     val foodList = ArrayList<RestaurantData>()
+    val arrayList = ArrayList<RestaurantData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant)
 
+        getDataOnFirebase()
+
+        restaurantSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                search(query.toString())
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                search(newText.toString())
+                return true
+            }
+        })
+
+    }
+
+
+    fun search(value:String){
+        if (value.isNotEmpty()){
+            foodList.clear()
+            // นำมาปรับตัวให้เป็นตัวเล็กทั้งหมด
+            val search = value.toLowerCase(Locale.getDefault())
+            arrayList.forEach {
+                // นำมาปรับตัวให้เป็นตัวเล็กทั้งหมด และนำมาเทียบกันว่ามีประโยคไหนเหมือนกันก็ให้โชว์
+                if (it.name.toLowerCase(Locale.getDefault()).contains(search)){
+                    foodList.add(it)
+                }
+            }
+            restaurantRecyclerView.adapter!!.notifyDataSetChanged()
+        }else{
+            foodList.clear()
+            foodList.addAll(arrayList)
+            restaurantRecyclerView.adapter!!.notifyDataSetChanged()
+
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+    private fun getDataOnFirebase(){
         db.collection("restaurant")
             .get().addOnCompleteListener {
                 if (it.isSuccessful){
@@ -32,20 +75,22 @@ class RestaurantActivity : AppCompatActivity(), onCLickAdapterListener {
                         val rating = document.get("rating").toString()
                         val telephone = document.getString("tel")
                         val map = document.getString("map")
-
-
-                        foodList.add(RestaurantData("$image","$title", rating,"$description", "$telephone", "$map"))
+                        arrayList.add(RestaurantData("$image", "$title", rating, "$description", "$telephone", "$map"
+                            )
+                        )
                     }
-                    restaurantRecyclerView.adapter = RestaurantAdapter(foodList,this)
+                    foodList.addAll(arrayList)
+                    restaurantRecyclerView.adapter = RestaurantAdapter(foodList, this)
                     restaurantRecyclerView.layoutManager = LinearLayoutManager(this)
+
                 }
             }
     }
 
     override fun onClick(postion: Int) {
-        val intent = Intent(this,DetailRestaurantActivity::class.java)
+        val intent = Intent(this, DetailRestaurantActivity::class.java)
         val bundle = Bundle()
-        bundle.putParcelable("restaurant",foodList[postion])
+        bundle.putParcelable("restaurant", foodList[postion])
         intent.putExtras(bundle)
         startActivity(intent)
     }
