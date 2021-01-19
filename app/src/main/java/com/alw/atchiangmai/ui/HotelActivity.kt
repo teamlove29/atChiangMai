159fadc9f51b60787baa410babe8e4376359b491
Yo.stnd <sing.tnd@gmail.com>
2021-01-19 15:55:53 +0700

    ++ drawable HotelActivity

--- app/src/main/java/com/alw/atchiangmai/ui/HotelActivity.kt
+++ app/src/main/java/com/alw/atchiangmai/ui/HotelActivity.kt
@@ -46,6 +46,23 @@ class HotelActivity : AppCompatActivity() {
                 cal.get(Calendar.DAY_OF_MONTH)
             ).show()
         }
+        val dateSetListener2 =
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.US)
+                editTextDate2.setText(sdf.format(cal.time))
+
+            }
+        editTextDate2.setOnClickListener {
+            DatePickerDialog(
+                this, dateSetListener2,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
     }
 
     private fun getFavHotel() {
