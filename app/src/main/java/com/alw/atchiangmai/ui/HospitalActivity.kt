555169279790ae302c0e01f1beefcc55d536a36c
NanthawatKTY <nanthawat.tae123@gmail.com>
2021-01-20 19:08:28 +0700

    Added SearchView and Deleted it and will add it again soon

--- app/src/main/java/com/alw/atchiangmai/ui/HospitalActivity.kt
+++ app/src/main/java/com/alw/atchiangmai/ui/HospitalActivity.kt
@@ -2,23 +2,21 @@ package com.alw.atchiangmai.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
-import android.widget.ProgressBar
+import android.widget.EditText
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.alw.atchiangmai.Adapter.HospitalAdapter
-import com.alw.atchiangmai.Adapter.OTOP_Adapter
 import com.alw.atchiangmai.FirebaseController
 import com.alw.atchiangmai.Model.Hospital_Model
-import com.alw.atchiangmai.Model.OTOP_Model
 import com.alw.atchiangmai.R
 import kotlinx.android.synthetic.main.activity_hospital.*
-import kotlinx.android.synthetic.main.activity_otop.*
-import retrofit2.Retrofit
+import java.util.*
+import kotlin.collections.ArrayList
 
 class HospitalActivity: AppCompatActivity() {
 
@@ -29,12 +27,6 @@ class HospitalActivity: AppCompatActivity() {
         val INTENT_PARCELABLE_hospital = "OBJECT_INTENT"
     }
 
-    // Progress Bar
-//    private lateinit var nestedScrollViewHospital: NestedScrollView
-//    private lateinit var recyclerViewHospital: RecyclerView
-//    private lateinit var progressBarHospital: ProgressBar
-//    private var page = 1
-//    private var limit = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,37 +34,53 @@ class HospitalActivity: AppCompatActivity() {
 
         // Firestore Collection
         val collectionHospitalFirestore = "hospital"
-        getFirestoreHospitalResult(collectionHospitalFirestore)
+        getFirestoreHospitalResult(collectionHospitalFirestore )
+
+
     }
+
 
     /// Get data once from Firestore
     private fun getFirestoreHospitalResult(collection: String) {
-        FirebaseController.Firebase.db.collection(collection)
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    Log.e(TAG, "${document.id} => ${document.data}")
-                    val hospitalImages = document.getString("image")
-                    val hospitalName = document.getString("name")
-                    val hospitalDescription = document.getString("des")
-                    val hospitalAddress = document.getString("add")
-                    val hospitalTel = document.getString("tel")
-                    when (collection) {
-                    //   "otopFood" -> otopLists.add(OTOP_Model("$otopImages", "$otopText").toString())
-                        "hospital" -> hospitalList.add(Hospital_Model("$hospitalImages", "$hospitalName", "$hospitalDescription", "$hospitalAddress", "$hospitalTel"))
+
+        //If search text is empty will display all docs from hospital collection
+
+            FirebaseController.Firebase.db.collection(collection)
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        Log.e(TAG, "${document.id} => ${document.data}")
+                        val hospitalImages = document.getString("image")
+                        val hospitalName = document.getString("name")
+                        val hospitalDescription = document.getString("des")
+                        val hospitalAddress = document.getString("add")
+                        val hospitalTel = document.getString("tel")
+                        when (collection) {
+                            //   "otopFood" -> otopLists.add(OTOP_Model("$otopImages", "$otopText").toString())
+                            "hospital" -> hospitalList.add(
+                                Hospital_Model(
+                                    "$hospitalImages",
+                                    "$hospitalName",
+                                    "$hospitalDescription",
+                                    "$hospitalAddress",
+                                    "$hospitalTel"
+                                )
+                            )
+                        }
+                    }
+                    rvHospital_Lists.layoutManager = LinearLayoutManager(this)
+                    rvHospital_Lists.setHasFixedSize(true)
+                    rvHospital_Lists.adapter = HospitalAdapter(this, hospitalList) {
+                        //       Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, HospitalDetailActivity::class.java)
+                        intent.putExtra(INTENT_PARCELABLE_hospital, it)
+                        startActivity(intent)
                     }
                 }
-                rvHospital_Lists.layoutManager = LinearLayoutManager(this)
-                rvHospital_Lists.setHasFixedSize(true)
-                rvHospital_Lists.adapter = HospitalAdapter(this, hospitalList){
-             //       Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show()
-                    val intent = Intent(this, HospitalDetailActivity::class.java)
-                    intent.putExtra(INTENT_PARCELABLE_hospital, it)
-                    startActivity(intent)
+                .addOnFailureListener { exception ->
+                    Log.d(TAG, "Error getting documents: ", exception)
                 }
-            }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "Error getting documents: ", exception)
-            }
+//        }
     }
+
 }
